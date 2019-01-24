420162d0a3ef93a22803d1fd65c5038cdc69c22f
drkfogle <rodriguez.kevin.094@gmail.com>
2019-01-24 16:17:18 -0500

    classes_work

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,135 @@
 // CODE here for your Lambda Classes
+
+class Person{
+    constructor(attrs){
+        this.name = attrs.name;
+        this.age = attrs.age;
+        this.location = attrs.location;
+        this.gender = attrs.gender;
+    }
+    speak(){
+        return  (`Hello my name is ${this.name} I am from ${this.location}.`)
+    }
+}
+
+class Instructor extends Person{
+    constructor(Iattrs){
+        super(Iattrs);
+        this.specialty = Iattrs.specialty;
+        this.favLanguage = Iattrs.favLanguage;
+        this.catchPhrase = Iattrs.catchPhrase;
+    }
+    demo(){
+        return (`Today we are learning about ${this.specialty}.`)
+    }
+    grade(){
+        return (`${student.name} receives a perfect score on ${this.specialty}.`)
+    }
+}
+
+class Student extends Person{
+    constructor(Sattrs){
+        super(Sattrs);
+        this.previousBackground = Sattrs.previousBackground;
+        this.className = Sattrs.className;
+        this.favSubjects = Sattrs.favSubjects;
+    }
+    listsSubjects(){
+        return (`${this.favSubjects}`)
+    }
+    PRAssignment (){
+        return (`${student.name} has submitted a PR for ${this.favSubjects}`)
+    }
+    sprintChallenge(){
+        return (`${student.name} has begun sprint challenge on ${this.favSubjects}`)
+    }
+}
+
+class ProjectManager extends Instructor{
+    constructor (pattrs){
+        super(pattrs);
+        this.gradClassName = pattrs.gradClassName;
+        this.favInstructor = pattrs.favInstructor;
+    }
+    standUp(){
+        return (`${this.name} announces to ${this.gradClassName}, @channel standup time!`)
+    }
+    debugsCode(){
+        return (`${this.name} debugs ${student.name}'s code on ${this.favSubjects}.`)
+    }
+}
+
+
+const fred = new Student({
+    name: 'Fred',
+    age: 34,
+    location: 'Albuquerque',
+    gender: 'Male',
+    previousBackground: 'Teacher',
+    className: 'fsd12',
+    favSubjects: 'Python, Ruby, Elixir, JS.'
+    
+})
+
+const tim = new Student({
+    name: 'Tim',
+    age: 18,
+    location: 'Oswego',
+    gender: 'Male',
+    previousBackground: 'Stock Boy',
+    className: 'cs13',
+    favSubjects: 'Spanish, English, French, CompSci.'
+})
+
+const tina = new Student({
+    name: 'Tina',
+    age: 19,
+    location: 'New Jersey',
+    gender: 'Female',
+    previousBackground: 'Cashier',
+    favSubjects: 'Engineering, Shop, Physics, Python, C#'
+})
+
+const rick = new Instructor({
+    name: 'Rick',
+    age: 43,
+    location: 'Who Knows?',
+    gender: 'Male',
+    specialty: 'Science',
+    favLanguage: 'Drunken English',
+    catchPhrase: 'IM PICKLE RICCCCCKKKKK!'
+})
+
+const jimbo = new ProjectManager({
+    name: 'Jim',
+    age: 21,
+    location: 'Madagascar',
+    gender: 'Male',
+    specialty: 'Not getting eaten by wild animals.',
+    favLanguage: 'PHP',
+    catchPhrase: 'Well, that just happened.',
+    gradClassName: 'fsw1',
+    favInstructor: 'Philip'
+})
+
+const terry = new ProjectManager({
+    name: 'Terry',
+    age: 25,
+    location: 'Philadelphia',
+    gender: 'Male',
+    specialty: 'Sleep',
+    favLanguage: 'Swift',
+    catchPhrase: 'SNORE',
+    favInstructor: 'Life',
+    gradClassName: 'fsw2'
+})
+
+const phil = new Instructor({
+    name: 'Philip',
+    age: 22,
+    location: 'California',
+    gender: 'Male',
+    specialty: 'Waking up Terry',
+    favLanguage: 'JS',
+    catchPhrase: '*bullhorn* hey, you awake Terry?'
+})
--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -1,9 +1,71 @@
-/* 
 
-Prototype Refactor
 
-1. Copy and paste your code or the solution from yesterday
+// Prototype Refactor
 
-2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
+// 1. Copy and paste your code or the solution from yesterday
 
-*/
+// function GameObject (attrs){
+// this.dimensions = attrs.dimensions;
+// this.createdAt = attrs.createdAt;
+// this.name = attrs.name;
+// }
+// GameObject.prototype.destroy = function (){
+//     return `${this.name} was removed from the game.`
+// }
+
+// function CharacterStats (attrs){
+//     GameObject.call(this, attrs);
+//     this.healthPoints = attrs.healthPoints;
+//     this.name = attrs.name;
+//     }
+//     CharacterStats.prototype = Object.create(GameObject.prototype);
+
+//     CharacterStats.prototype.takeDamage = function () {
+//     return (`${this.name} took damage.`)
+//     }
+
+
+// function Humanoid(attrs){
+//     CharacterStats.call(this,attrs);
+//     this.team = attrs.team;
+//     this.weapons = attrs.weapons;
+//     this.language = attrs.language;
+//     }
+//     Humanoid.prototype = Object.create(CharacterStats.prototype);
+//     Humanoid.prototype.greet = function (){
+//     return (`${this.name} offers a greeting in ${this.language}.`)
+//     }
+
+// 2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
+
+class GameObject{
+    constructor (attrs) {
+        this.dimensions = attrs.dimensions;
+        this.createdAt = attrs.createdAt;
+    }
+    destroy (){
+        console.log(`{this.name} was removed from the game.`)
+    }
+}
+
+class CharacterStats extends GameObject{
+    constructor (statsattrs){
+        super(statattrs);
+        this.healthpoints = statsattrs.healthpoints;
+        this.name = statsattrs.name;
+    }
+    takeDamage(){
+        return (`${this.name} took damage.`)
+    }
+}
+
+class Humanoid extends CharacterStats{
+    super(Hattrs){
+        this.team = Hattrs.team;
+        this.weapons = Hattrs.weapons;
+        this.language = Hattrs.language;
+    }
+    greet(){
+        return (`${this.name} offers a greeting in ${this.language}.`)
+    }
+}
